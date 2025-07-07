7fa3fef7cb5847f9cce511c0366cb295530a7bdf
aogp1975 <2193038392@correo.ler.uam.mx>
2025-07-07 12:40:14 -0600

    prueba img

--- desk.js
+++ desk.js
@@ -49,7 +49,7 @@ function initmap (center, showmarcador = false) {
   });
 
   map.on('load', function () {
-    map.loadImage('https://backend-vt.onrender.com/pasamos.png', function (error, image) {
+    map.loadImage('https://github.com/aogp1975/violenciastransitorias/blob/main/assets/images/icon1.png', function (error, image) {
       if (error) return console.error(error);
       map.addImage('pasamos', image);
 
